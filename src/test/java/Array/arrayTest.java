c9f638e5e68b9229bd083f36e802be6dbf34a2f8
zxw1046137644 <Zxw1997@>
2022-09-16 20:08:51 +0800

    Thread

--- src/test/java/Array/arrayTest.java
+++ src/test/java/Array/arrayTest.java
@@ -1,12 +1,24 @@
 package Array;
 
+import org.apache.poi.util.ArrayUtil;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class arrayTest {
 
     int[] intValue = new int[]{1,234,4,3,5,5};
     @Test
     public void Test(){
-        System.out.println();
+        System.out.println(intValue);
+        Arrays.sort(intValue);
+        for (int i =intValue.length-1;i>=0;i--){
+            System.out.println(intValue[i]);
+        }
+
     }
+
+
+
+
 }
